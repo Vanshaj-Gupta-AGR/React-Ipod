b597f4f35de6c26ef709508fa53f52a1bdc1952b
Vanshaj Gupta <vanshaj444@gmail.com>
2022-01-12 15:10:16 +0530

    components done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Ipod from './Ipod';
+import Settings from './Settings';
+import Games from './Games';
+import Artists from './Artists';
+import Music from './Music';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Music />
+
     </div>
   );
 }
